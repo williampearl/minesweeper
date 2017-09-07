47cbce54d3b2371288a5c77ac94f24f2fefda6fe
William <will.pearl200@gmail.com>
2017-09-06 20:04:48 -0500

    Add files via upload
    wow i just committed 3 times and it didnt work

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,6 +1,7 @@
 import java.util.*;
-import java.awt.Graphics;
+import java.awt.*;
 import java.io.*;
+
 public class Minesweeper {
 
 	public static void main(String[] args)
@@ -45,6 +46,9 @@ public class Minesweeper {
 		
 	}
 	
+	/**
+	 * Works the game clock and input
+	 */
 	public void runIt()
     {
     	while(runGame)
@@ -59,11 +63,13 @@ public class Minesweeper {
     	}
     }
 	
+	/**
+	 * Sets up a new game
+	 * @param sizeX - the amount of columns
+	 * @param sizeY - the amount of rows
+	 */
 	public void startGame(int sizeX, int sizeY)
 	{
-//		mapD = new boolean[sizeY][sizeX];
-//		map = new boolean[sizeY][sizeX];
-//		am = new int[sizeY][sizeX];
 		map = new Tile[sizeX][sizeY];
 		runGame = true;
 		debug = false;
@@ -75,6 +81,10 @@ public class Minesweeper {
 		setBombs();
 		runIt();
 	}
+	
+	/**
+	 * Receives input from stdin and sends it to parser
+	 */
 	public void getInput()
 	{
 		
@@ -84,23 +94,35 @@ public class Minesweeper {
 		parseString(line);
 		//in.close();
 	}
-
+	
+	/**
+	 * Parses and runs commands
+	 * @param l
+	 */
 	public void parseString(String l){
 		String[] s = l.split(" ");
 		if(s.length == 1){
 			switch(l)
 			{
+			/*help screen*/
 			case "h": printHelp(); break;
+			/*quit game*/
 			case "q": endGame(false); break;
-			case "p": System.out.println(printMap(false)); break;
+			/*print the map*/
+			case "p": showMap(); break;
+			/*prints the whole map*/
 			case "sp": System.out.println(printMap(true)); break;
+			/*toggles debug mode*/
 			case "d": debug = !debug; break;
 			}
 		} else {
+			//these require additional input so they are separated
 			switch(s[0])
 			{
-			case "f": parseFlag(l); if(!runGame)break; System.out.println(printMap(false)); break;
-			case "c": parseClick(l); if(!runGame)break; System.out.println(printMap(false)); break;
+			/*place flag*/
+			case "f": parseFlag(l); if(!runGame)break; showMap(); break;
+			/*click spot*/
+			case "c": parseClick(l); if(!runGame)break; showMap(); break;
 			}
 		}
 		
@@ -108,24 +130,45 @@ public class Minesweeper {
 	public void parseClick(String line)
 	{
 		String[] l = line.split(" ");
-		click(Integer.parseInt(l[1]), Integer.parseInt(l[2]));
+		//if they didnt input 2 integers then it fails
+		try {
+			click(Integer.parseInt(l[1]), Integer.parseInt(l[2]));
+		} catch (NumberFormatException e) {
+			System.err.println("You didn't input 2 numbers.");
+		}
 	}
 	public void parseFlag(String line)
 	{
+		
 		String[] l = line.split(" ");
-		//long ct = System.currentTimeMillis();
-		flag(Integer.parseInt(l[1]), Integer.parseInt(l[2]));
+		//if they didn't input 2 integers then it fails
+		try {
+			flag(Integer.parseInt(l[1]), Integer.parseInt(l[2]));
+		} catch (NumberFormatException e) {
+			System.err.println("You didn't input 2 numbers.");
+		}
+		
 		
 	}
 	
-	
-	public void incrementTileAmount(int r, int c)
+	/**
+	 * increments amount of bombs surrounding the tile
+	 * @param r the row
+	 * @param c the column
+	 */
+	private void incrementTileAmount(int r, int c)
 	{
+		//checks if location is out of bounds
 		if((r < 0 || r >= map.length) || (c < 0 || c >= map[r].length))
 			return;
 		map[r][c].incrementAmount();
 	}
-	public void incrementArea(int r, int c)
+	/**
+	 * increments the area around a bomb
+	 * @param r the row
+	 * @param c the column
+	 */
+	private void incrementArea(int r, int c)
 	{
 		incrementTileAmount(r-1, c-1);
 		incrementTileAmount(r-1, c);
@@ -137,27 +180,52 @@ public class Minesweeper {
 		incrementTileAmount(r+1, c+1);
 	}
 	
+	/**
+	 * area of the board
+	 * X * Y
+	 * @return area
+	 */
 	public int getArea() {
 		return map.length * map[0].length;
 	}
-	public void setBomb()
+	
+	/**
+	 * places a bomb in a random spot
+	 */
+	private void setBomb()
 	{
+		//pick a random row
 		int r = ((int)(Math.random()*getArea()))%map.length;
+		//pick a random column
 		int c = ((int)(Math.random()*getArea()))%map[0].length;
-		if(map[r][c].isBomb())
+		//check if picked spot already has a bomb
+		if(map[r][c].isBomb()) {
 			setBomb();
+			return;
+		}
 		map[r][c].setBomb();
+		
 		incrementArea(r,c);
 		
 		
 	}
+	
+	/**
+	 * places bombs
+	 * add "int numOfBombs"
+	 */
 	public void setBombs()
 	{
+		//change the 25 to numOfBombs
 		for(int i=0; i<25; i++)
 			setBomb();
 	}
 	
-	
+	/**
+	 * flag given spot
+	 * @param r the row
+	 * @param c the column
+	 */
 	public void flag(int r, int c)
 	{
 		map[r][c].setFlagged();
@@ -165,34 +233,55 @@ public class Minesweeper {
 			endGame(true);
 	}
 	
-	
+	/**
+	 * clicks spot
+	 * @param r the row
+	 * @param c the column
+	 */
 	public void click(int r, int c)
 	{
+		//diagnostic printing
 		if(debug)
 		{
+			//shows the spot clicked and stats about it
 			System.out.printf("Click @ %d : %d\n",r,c);
 			System.out.printf("%d, %d is " + (map[r][c].isBomb()?"":"not")+" a bomb.\n", r, c);
 		}
+		//check if clicked a bomb
 		if(map[r][c].isBomb())
 		{
+			//clicked bomb, lost game
 			endGame(false);
 			return;
 		}
+		//check if spot is already discovered
 		if(map[r][c].isDiscovered())
 		{
+			//give error
 			System.err.println("Clicked on spot already discovered.");
 			return;
 		}
+		
 		autoClick(r,c);
 			
 	}
-	public void autoClick(int r, int c)
+	/**
+	 * discovers blank tiles
+	 * @param r
+	 * @param c
+	 */
+	private void autoClick(int r, int c)
 	{
+		//checks if out of bounds
 		if(((r < 0 || r >= map.length) || (c < 0 || c >= map[r].length)) || map[r][c].isBomb() || map[r][c].isDiscovered())
 			return;
+		//diagnostic stuff
 		if(debug)
 			System.out.printf("Autoclick @ %d : %d\n", r, c);
+		
 		map[r][c].discoverTile();
+		
+		//if amount != 0 then the spot is adjacent to a bomb, so dont continue
 		if(map[r][c].getAmount() != 0)
 			return;
 		autoClick(r-1, c);
@@ -201,7 +290,10 @@ public class Minesweeper {
 		autoClick(r, c+1);
 	}
 	
-	
+	/**
+	 * checks if all the bombs have been flagged
+	 * @return
+	 */
 	public boolean checkWin()
 	{
 		boolean d = true;
@@ -211,6 +303,10 @@ public class Minesweeper {
 					d = false;
 		return d;
 	}
+	/**
+	 * ends the game
+	 * @param win true = won, false = lost
+	 */
 	public void endGame(boolean win)
 	{
 		if(win)
@@ -223,7 +319,7 @@ public class Minesweeper {
 			System.out.println("Game over!");
 			
 			System.out.println("What you discovered.");
-			System.out.println(printMap(false));
+			showMap();
 			
 			System.out.println("\n\nThe whole map.");
 			System.out.println(printMap(true));
@@ -231,7 +327,18 @@ public class Minesweeper {
 		runGame = false;
 	}
 	
+	/**
+	 * prints map to stdout
+	 */
+	public void showMap() {
+		System.out.println(printMap(false));
+	}
 	
+	/**
+	 * gives a nice formatted view of the map
+	 * @param showAll
+	 * @return formatted map
+	 */
 	public String printMap(boolean showAll)
 	{
 		String o = "  ";
@@ -267,17 +374,30 @@ public class Minesweeper {
 		}
 		return o;
 	}
+	
+	/**
+	 * gives map for use with gui
+	 * @return
+	 */
+	public Tile[][] getMap() {
+		return map;
+	}
+	
+	/**
+	 * prints the help screen
+	 */
 	public void printHelp()
 	{
 		try {
+			//access help screen file
 			Scanner f = new Scanner(new File("HelpScreen.txt"));
+			//print the file
 			while(f.hasNextLine())
 				System.out.println(f.nextLine());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
-			System.out.println("Your help screen file is missing.");
-			return;
+			System.err.println("Your help screen file is missing.");
 		}
 		
 		
--- Tile.java
+++ Tile.java
@@ -40,33 +40,55 @@ public class Tile {
 	{
 		amount = am;
 	}
+	/**
+	 * has the tile been discovered
+	 * @return
+	 */
 	public boolean isDiscovered()
 	{
 		return disc;
 	}
-	
+	/**
+	 * makes the tile a bomb
+	 */
 	public void setBomb()
 	{
 		bomb = true;
 	}
+	/**
+	 * is the tile a bomb
+	 * @return
+	 */
 	public boolean isBomb()
 	{
 		return bomb;
 	}
-	
+	/**
+	 * return amount of bombs nearby
+	 * @return
+	 */
 	public int getAmount()
 	{
 		return amount;
 	}
+	/**
+	 * adds 1 to amount of nearby bombs
+	 */
 	public void incrementAmount()
 	{
 		setAmount(amount + 1);
 	}
-	
+	/**
+	 * is the tile flagged
+	 * @return
+	 */
 	public boolean isFlagged()
 	{
 		return flag;
 	}
+	/**
+	 * toggles flagged state
+	 */
 	public void setFlagged()
 	{
 		flag = !flag;
